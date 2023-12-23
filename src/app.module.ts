bfac23df7b53ffbdb3b5b9da2d7d65d59f591f91
Ridwan Taiwo <donriddo@gmail.com>
2023-12-23 13:57:57 +0100

    chore: setup env config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+
+const configSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRATION_TIME: Joi.string().default('60s'),
+});
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    NotesModule,
+    ConfigModule.forRoot({
+      validationSchema: configSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
