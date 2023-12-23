1ef5ad5fddfb3c7f8c386dfedf4bd0c5de25576e
Ridwan Taiwo <donriddo@gmail.com>
2023-12-23 15:21:39 +0100

    feat: user module with mongoose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 const configSchema = Joi.object({
   PORT: Joi.number().default(3000),
@@ -22,6 +24,23 @@ const configSchema = Joi.object({
     ConfigModule.forRoot({
       validationSchema: configSchema,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get<string>('DB_HOST');
+        const port = configService.get<number>('DB_PORT');
+        const user = configService.get<string>('DB_USER');
+        const password = configService.get<string>('DB_PASSWORD');
+        const dbName = configService.get<string>('DB_NAME');
+
+        return {
+          uri: `mongodb://${user}:${password}@${host}:${port}`,
+          dbName: dbName,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
